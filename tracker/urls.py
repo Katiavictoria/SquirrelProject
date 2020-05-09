593bee46eff84fb8e41b4b89f2450d152efc8651
Katia Basmaci <katia.basmaci@gmail.com>
2020-05-09 19:41:20 +0000

    Edit tracker urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('map/', views.map, name='map'),
-    path('sightings/', views.list, name='list'),
-    path('sightings/add/', views.create, name='create'),
-    path('sightings/stats/', views.stats, name='stats'),
-    path('sightings/<str:unique_squirrel_id>/', views.update, name='update'),
-    path('sightings/<str:unique_squirrel_id>/delete', views.delete, name='delete'),
-]
-
+    path('sightings/', views.sightings),
+    path('sightings/<str:UniqueID>/', views.update_sighting, name = 'update'),
+    path('sightings/add/', views.sightings_add, name='add'),
+    path('sightings/stats/', views.sightings_stats),
+    path('sightings/<unique_squirrel_id>/', views.sightings_edit, name='edit'),
+    path('sightings/map/',views.map),
+ ]
