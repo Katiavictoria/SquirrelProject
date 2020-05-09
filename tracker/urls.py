d2243fde7e16c29307de127c5cd1cf6997755dfe
Katia Basmaci <katia.basmaci@gmail.com>
2020-05-09 20:02:45 +0000

    Change urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sightings),
-    path('<str:UniqueID>/', views.update_sighting, name = 'update'),
-    path('add/', views.sightings_add, name='add'),
-    path('stats/', views.sightings_stats),
+    path('', views.tracker),
+    path('<str:UniqueID>/', views.update_tracker, name = 'update'),
+    path('add/', views.add, name='add'),
+    path('stats/', views.stats),
     path('<unique_squirrel_id>/', views.sightings_edit, name='edit'),
     path('map/',views.map),
  ]
