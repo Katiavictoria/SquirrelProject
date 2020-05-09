1a4eba0c003b21556b5ea16805acb76557ebb255
Katia Basmaci <katia.basmaci@gmail.com>
2020-05-09 20:48:31 +0000

    Add views.

--- tracker/views.py
+++ tracker/views.py
@@ -1,98 +1,61 @@
+from django.shortcuts import render, redirect
 
-from django.shortcuts import render
+from .models import Sightings
+
+from .forms import SightingsForm
+
+from django.core.exceptions import MultipleObjectsReturned
+
+from django.db.models import Count
 from django.http import HttpResponse
 
-from django.shortcuts import render, redirect
-from .forms import SquirrelForm, SquirrelForm1
+def sightings(request):
 
-from .models import Squirrel
+    all_sightings = Sightings.objects.all()
+
+    context = {
+
+            'all_sightings': all_sightings,
+    }
+
+    return render(request,'tracker/sightings.html',context)
+
+def sightings_add(request):
+
+    form = SightingsForm()
+
+context = {
+
+        'form' : form,
+    }
+
+return render(request, 'tracker/add.html', context)
+
+def sightings_stats(request):
+    all_sightings = Sightings.objects.all()
+    #context = {
+    #        'all_sightings': all_sightings,
+    #}
+    return render(request,'tracker/stats.html',context)
+
+def sightings_stats(request):
+    squirrel_age = Sightings.objects.values('age').order_by('age').annotate(age_count=Count('age'))
+    squirrel_primary_fur_color = Sightings.objects.values('primary_fur_color').order_by('primary_fur_color').annotate(primaryfurcolor_count=Count('primary_fur_color'))
+    squirrel_climbing =  Sightings.objects.values('climbing').order_by('climbing').annotate(climbing_count=Count('climbing'))
+    squirrel_chasing = Sightings.objects.values('chasing').order_by('chasing').annotate(chasing_count=Count('chasing'))
+    squirrel_running = Sightings.objects.values('running').order_by('running').annotate(running_count=Count('running'))
+    context = {
+        'squirrel_age': squirrel_age,
+        'squirrel_primary_fur_color': squirrel_primary_fur_color,
+        'squirrel_climbing': squirrel_climbing,
+        'squirrel_chasing': squirrel_chasing,
+        'squirrel_running': squirrel_running,
+    }
+    return render(request, 'tracker/stats.html', context)
 
 def map(request):
-    sightings0 = Squirrel.objects.all()
-    # sightings = sightings0[:10]
-    sightings_loc_ = []
-    sightings = []
-    for i in range(120):
-        sightings_loc = [sightings0[i].Latitude, sightings0[i].Longitude]
-        if sightings_loc not in sightings_loc_:
-            sightings_loc_.append(sightings_loc)
-            sightings.append(sightings0[i])
-        if len(sightings) == 100:
-            break
-    context = {'sightings': sightings}
-    return render(request, 'tracker/map.html', context)
-
-def list(request):
-    squirrel_list = Squirrel.objects.all()
-    context = {'squirrel_list': squirrel_list}
-    return render(request, 'tracker/list.html', context)
-
-def update(request, unique_squirrel_id):
-    squirrel = Squirrel.objects.get(Unique_Squirrel_ID=unique_squirrel_id)
-    if request.method == 'GET':
-        form = SquirrelForm(instance=squirrel)
-        return render(request, 'tracker/form.html', {'form': form})
-    else:
-        form = SquirrelForm(request.POST, instance=squirrel)
-        if form.is_valid():
-            form.save()
-        return redirect('/sightings')
-
-def create(request):
-    if request.method == 'GET':
-        form = SquirrelForm1()
-        return render(request, 'tracker/form1.html', {'form': form})
-
-
-def delete(request, unique_squirrel_id):
-    squirrel = Squirrel.objects.get(Unique_Squirrel_ID=unique_squirrel_id)
-    squirrel.delete()
-    return redirect('/sightings')
-
-def stats(request):
-    squirrel_list = Squirrel.objects.all()
-    age_ = []
-    forage_ = []
-    location_ = []
-    other_activities_ = []
-    primary_fur_color_ = []
-    for squirrel in squirrel_list:
-        age_.append(squirrel.Age)
-        forage_.append(squirrel.Foraging)
-        location_.append(squirrel.Location)
-        other_activities_.append(squirrel.Other_Activities)
-        primary_fur_color_.append(squirrel.Primary_Fur_Color)
-
-    def mode_(lst):
-        dic1={}
-        s=set(lst)
-        for i in s:
-            ct=lst.count(i)
-            dic1[i]=ct
-        return max(dic1,key=dic1.get)
-
-    def count_(lst):
-        return len([x for x in lst if x])
-
-    def percentage_(lst):
-        ct = 0
-        for i in range(0,len(lst)):
-            if lst[i] == True:
-                ct += 1
-        per = ct/len(lst)
-        return per
-
-    Mage =mode_(age_)
-    Pforage = percentage_(forage_)
-    NLoca = count_(location_)
-    NOA = count_(other_activities_)
-    NPFC = count_(primary_fur_color_)
-    dict_ = {
-        'Most_frequent_age': Mage,
-        'P_forage': Pforage,
-        'Count_of_Location': NLoca,
-        'Count_of_Other_Activities': NOA,
-        'Count_of_Primary_Fur_Color': NPFC
+    all_sightings = Sightings.objects.all()[:100]
+    context = {
+            'all_sightings': all_sightings,
     }
-    return render(request, 'tracker/stats.html', dict_)
-
+    return render(request,'tracker/map.html',context)
