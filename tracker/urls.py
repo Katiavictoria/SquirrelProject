3e0a9f842c3471e730224d58e9ef5c97eb36b8b0
Katia Basmaci <katia.basmaci@gmail.com>
2020-05-09 20:03:49 +0000

    Done

--- tracker/urls.py
+++ tracker/urls.py
@@ -3,10 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tracker),
-    path('<str:UniqueID>/', views.update_tracker, name = 'update'),
     path('add/', views.add, name='add'),
     path('stats/', views.stats),
-    path('<unique_squirrel_id>/', views.sightings_edit, name='edit'),
     path('map/',views.map),
  ]
